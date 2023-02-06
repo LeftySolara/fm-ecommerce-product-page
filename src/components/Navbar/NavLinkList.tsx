4d570aca376237087d63facd536fdb2733ff2ac9
Julianne Adams <julianne@julianneadams.info>
2023-02-06 15:19:43 -0500

    Add: avatar and shopping cart icon

--- src/components/Navbar/NavLinkList.tsx
+++ src/components/Navbar/NavLinkList.tsx
@@ -7,7 +7,7 @@ interface NavListProps {
 const NavLinkList = (props: NavListProps) => {
   const { items } = props;
   return (
-    <ul className="flex w-1/2 flex-none flex-row flex-nowrap justify-evenly">
+    <ul className="flex w-96 flex-none flex-row flex-nowrap justify-evenly">
       {items.map((item) => (
         <NavLink text={item} key={item} />
       ))}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,21 @@
 import NavLinkList from "./NavLinkList";
 import Logo from "./Logo";
+import CartIcon from "./CartIcon";
+import Avatar from "../../assets/images/image-avatar.png";
 
 const Navbar = () => {
   const navItems = ["Collections", "Men", "Women", "About", "Contact"];
 
   return (
-    <div className="flex h-24 w-10/12 flex-row items-center justify-start border-b-2 border-solid border-blue-200">
-      <Logo />
-      <NavLinkList items={navItems} />
+    <div className="flex h-24 w-10/12 flex-row justify-between border-b-2 border-solid border-blue-200">
+      <div className="flex w-auto flex-row items-center justify-start">
+        <Logo />
+        <NavLinkList items={navItems} />
+      </div>
+      <div className="flex w-28 flex-row items-center justify-between">
+        <CartIcon />
+        <img src={Avatar} className="w-12" />
+      </div>
     </div>
   );
 };
